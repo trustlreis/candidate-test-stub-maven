f07f9081115f339b7fb19570b9b058a183451446
Luis Machado Reis <luis.reis@trustly.com>
2024-06-28 10:03:47 -0300

    - Fixing SampleTest to correct Assert message - Adding a substraction test

--- src/test/java/com/mycompany/app/SampleTest.java
+++ src/test/java/com/mycompany/app/SampleTest.java
@@ -9,7 +9,13 @@ public class SampleTest {
     @Test
     public void testAddition() {
         int sum = 2 + 2;
-        assertEquals(4, sum, "2 + 2 should equal 5");
+        assertEquals(4, sum, "2 + 2 should equal 4");
+    }
+
+    @Test
+    public void testSubstraction() {
+        int substraction = 4 - 2;
+        assertEquals(2, substraction, "4 - 2 should equal 2");
     }
 
 }
